b41a75a9bb545aa578664782394b9bd5df5f29a7
kjb4494 <rlagnltn1357@gmail.com>
2020-10-08 15:15:48 +0900

    restart!

--- lv1/q_01.py
+++
@@ -1,8 +0,0 @@
-# 행렬의 덧셈 lv1
-def sumMatrix(A,B):
-    answer = []
-    answer = [[c + d for c, d in zip(a, b)] for a, b in zip(A,B)]
-    return answer
-
-def q_01():
-    print(sumMatrix([[1,2], [2,3]], [[3,4],[5,6]]))
